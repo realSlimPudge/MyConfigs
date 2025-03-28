fd29ba8260a44571bed44428c4ca7535a038104d
realSlimPudge <komissaeov010806@gmail.com>
2025-03-28 18:23:43 +0300

    nvim

---
+++ nvim/lua/community.lua
@@ -0,0 +1,12 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.aurora" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.color.transparent-nvim" },
+}
